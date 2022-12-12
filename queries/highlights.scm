88e6047f3bb60b2a944d3aa1d41b1906ef084abb
liushuyu <liushuyu011@gmail.com>
2022-12-11 20:13:49 -0700

    queries: add syntax highlight queries

---
+++ queries/highlights.scm
@@ -0,0 +1,22 @@
+; Variables
+;----------
+
+(field_name) @variable
+
+; Literals
+;---------
+
+(comment) @comment
+
+[
+  (field_value)
+  (folded_line)
+] @string
+
+; Tokens
+;-------
+
+[
+  ":"
+  "."
+] @punctuation.delimiter
