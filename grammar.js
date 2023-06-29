789bd61ce71069cd4d440b3686a85de8f68f0a5c
liushuyu <liushuyu011@gmail.com>
2023-06-29 14:43:08 -0600

    grammar: make the field_value ...
    ... covers the full multiline values

--- grammar.js
+++ grammar.js
@@ -9,13 +9,13 @@ module.exports = grammar({
     field_name: ($) => /[!"$-,\.-9;-~][!-9;-~]*/,
     folded_line: ($) => seq($._hspace, /.+/),
     _hspace: ($) => /[ \t]+/,
-    statement: ($) =>
-      seq($.simple_line, repeat(choice($.escape_line, $.folded_line))),
-    paragraph: ($) => seq(repeat1($.statement), repeat($._separator_line)),
-    simple_line: ($) => seq($.field_name, ":", token(/\s*/), $.field_value),
-    _separator_line: ($) => token(/([ \t]+|\n)\n/),
+    paragraph: ($) => seq(repeat1($.statement), repeat1($._separator_line)),
+    field_value: ($) =>
+      seq($.simple_value, repeat(choice($.escape_line, $.folded_line))),
+    statement: ($) => seq($.field_name, ":", token(/\s*/), $.field_value),
+    _separator_line: ($) => /\n[ \t]*\n/,
     comment: ($) => token(seq("#", /.*/)),
     // catch-all
-    field_value: ($) => /.*/,
+    simple_value: ($) => /.*/,
   },
 });
