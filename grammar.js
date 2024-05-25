e6cc4c53cfad609d6690230aae6e3d91e25dc27c
liushuyu <liushuyu011@gmail.com>
2024-05-24 18:59:23 -0600

    grammar.js: use tree-sitter DSL as much as possible ...
    ... to improve error recovery

--- grammar.js
+++ grammar.js
@@ -1,18 +1,19 @@
 module.exports = grammar({
   name: "deb822",
   extras: ($) => [$.comment, "\n"],
-  conflicts: ($) => [[$.paragraph]],
   word: ($) => $.field_name,
-  externals: ($) => [$._separator_line, $.comment, $.simple_value],
+  externals: ($) => [$._separator_line],
   rules: {
     source: ($) => repeat($.paragraph),
     escape_line: ($) => seq($._hspace, "."),
     field_name: ($) => /[!"$-,\.-9;-~][!-9;-~]*/,
     folded_line: ($) => seq($._hspace, /.+/),
+    comment: ($) => seq("#", /[^\n]*/),
     _hspace: ($) => /[ \t]+/,
     paragraph: ($) => seq(repeat1($.statement), repeat1($._separator_line)),
+    simple_value: ($) => /[^\n]*/,
     field_value: ($) =>
       seq($.simple_value, repeat(choice($.escape_line, $.folded_line))),
-    statement: ($) => seq($.field_name, ":", /\s*/, $.field_value),
+    statement: ($) => seq($.field_name, ":", /[ \t]*/, $.field_value),
   },
 });
--- src/scanner.c
+++ src/scanner.c
@@ -4,8 +4,6 @@
 
 enum TokenType {
   TOKEN_PARAGRAPH_SEP = 0,
-  TOKEN_COMMENT = 1,
-  TOKEN_SIMPLE_VALUE = 2,
 };
 
 void *tree_sitter_deb822_external_scanner_create(void) { return NULL; }
@@ -20,12 +18,6 @@ unsigned tree_sitter_deb822_external_scanner_serialize(void *payload,
 void tree_sitter_deb822_external_scanner_deserialize(void *payload,
                                                      const char *buffer,
                                                      unsigned length) {}
-
-static inline void take_until_new_line(TSLexer *lexer) {
-  while (!lexer->eof(lexer) && lexer->lookahead != '\n') {
-    lexer->advance(lexer, false);
-  }
-}
 
 bool tree_sitter_deb822_external_scanner_scan(void *payload, TSLexer *lexer,
                                               const bool *valid_symbols) {
@@ -44,26 +36,11 @@ bool tree_sitter_deb822_external_scanner_scan(void *payload, TSLexer *lexer,
       case '\n':
         valid_separator = true;
         continue;
-      case '#':
-        lexer->result_symbol = TOKEN_COMMENT;
-        take_until_new_line(lexer);
-        lexer->mark_end(lexer);
-        return true;
       case '\0':
         return true;
       }
       return false;
     }
-  } else if (lexer->lookahead == '#' && !valid_symbols[TOKEN_SIMPLE_VALUE]) {
-    lexer->result_symbol = TOKEN_COMMENT;
-    take_until_new_line(lexer);
-    lexer->mark_end(lexer);
-    return true;
-  } else if (valid_symbols[TOKEN_SIMPLE_VALUE]) {
-    lexer->result_symbol = TOKEN_SIMPLE_VALUE;
-    take_until_new_line(lexer);
-    lexer->mark_end(lexer);
-    return true;
   }
 
   return false;
