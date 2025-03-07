02dcf4ae7eeabbcf29ab9bccbb6239e0c33b0317
liushuyu <liushuyu011@gmail.com>
2025-03-07 10:12:36 +0800

    tree-wide: bump version to 0.4.0

--- src/parser.c
+++ src/parser.c
@@ -769,7 +769,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_deb822(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 3,
+      .minor_version = 4,
       .patch_version = 0,
     },
   };
